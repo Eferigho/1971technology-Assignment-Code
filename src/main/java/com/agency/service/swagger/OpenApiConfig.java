d4d916a6349db3152e63635566f1df76ad20578a
eferigho <iovuefe@gmail.com>
2025-06-27 13:13:05 +0100

    Done with project

--- src/main/java/com/agency/service/swagger/OpenApiConfig.java
+++ src/main/java/com/agency/service/swagger/OpenApiConfig.java
@@ -12,7 +12,7 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("News API")
+                        .title("User Posting API")
                         .version("1.0")
                         .description("API for managing users and posts"));
     }
